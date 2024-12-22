756e2407ab7f94e134ea9b1333f1da935b11e006
Mubashshir Ali <jssmithsa6516@gmail.com>
2024-12-22 21:45:46 +0530

    Stateful Widget

---
+++ lib/dice_roller.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class DiceRoller extends StatefulWidget {
+  const DiceRoller({super.key});
+
+  @override
+  State<DiceRoller> createState() {
+    return _DiceRollerState();
+  }
+}
+
+class _DiceRollerState extends State<DiceRoller> {
+  String activeDice = 'assets/images/dice-3.png';
+
+  void rollDice() {
+    setState(() {
+      activeDice = 'assets/images/dice-2.png';
+    });
+  }
+
+  @override
+  Widget build(context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Image.asset(
+          activeDice,
+          width: 200,
+        ),
+        const SizedBox(
+          height: 20,
+        ),
+        TextButton(
+          onPressed: rollDice,
+          style: TextButton.styleFrom(
+            foregroundColor: Colors.white,
+            textStyle: const TextStyle(
+              fontSize: 28,
+            ),
+          ),
+          child: const Text('Roll the Dice!'),
+        )
+      ],
+    );
+  }
+}
--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,14 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:new_app/dice_roller.dart';
 // import 'package:new_app/styled_text.dart';
 
 // const startAlignment = Alignment.topLeft;
 // const endAlignment = Alignment.bottomRight;
 
 class GradientContainer extends StatelessWidget {
-  const GradientContainer(this.colorList, {required this.startFrom,required this.endTo,super.key});
+  const GradientContainer(this.colorList,
+      {required this.startFrom, required this.endTo, super.key});
 
-  const GradientContainer.purple({super.key}):colorList = const [ Color.fromARGB(255, 0, 0, 0), Color.fromARGB(255, 49, 10, 166)], startFrom = Alignment.topLeft, endTo = Alignment.bottomRight;
-  
+  const GradientContainer.purple({super.key})
+      : colorList = const [
+          Color.fromARGB(255, 0, 0, 0),
+          Color.fromARGB(255, 49, 10, 166)
+        ],
+        startFrom = Alignment.topLeft,
+        endTo = Alignment.bottomRight;
+
+
+
   final List<Color> colorList;
   final Alignment startFrom;
   final Alignment endTo;
@@ -17,10 +27,10 @@ class GradientContainer extends StatelessWidget {
   Widget build(context) {
     return Container(
       decoration: BoxDecoration(
-          gradient: LinearGradient(
-              colors: colorList, begin: startFrom, end: endTo)),
-      child: Center(
-        child: Image.asset('assets/images/dice-3.png', width: 200,),
+          gradient:
+              LinearGradient(colors: colorList, begin: startFrom, end: endTo)),
+      child: const Center(
+        child: DiceRoller(),
       ),
     );
   }
